50c88da1d6c08e1e1b31ffbf9297e78c5f50c04a
Yevheniia Nosova <zhenianosova2006@gmail.com>
2025-10-23 19:59:44 +0200

    16 new

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,9 @@
+#Studrnts   information ptogram
+
+name = input("What is ypur name?: ")
+age = input("How oldare you?: ")
+tall = input("what  is your height?: ")
+future = input("How  old will you be in 6 years?: ")
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {tall} cm.")
+print(f"In 6 years I will be {future} years old.")
---
+++ 02-TypesAndVariables/5-3.py
@@ -0,0 +1,12 @@
+###
+# A program that calculates the volume
+# and surface area of ​​a cuboid with sides a, b, and c.
+# Read the dimensions of the cuboid from the keyboard.
+#
+a = input('a= ')
+b = input("b= ")
+c = input("c= ")
+volume = a  * b * c
+surface_area =  2(a * b + a * c + b * c)
+print(f"volume of the cuboidwith sides a, b and  c is  {volume}")
+print(f"surface area of the cuboidwith sides a, b and  c is  {surface_area}")
