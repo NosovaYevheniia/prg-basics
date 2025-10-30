c089873987685801c12676546fb9bd0040f52f2d
Yevheniia Nosova <zhenianosova2006@gmail.com>
2025-10-30 19:07:09 +0100

    4 new

---
+++ 04-Functions/3-1.py
@@ -0,0 +1,11 @@
+###
+# To use the functions available in an external module,
+# you must include that module in your program.
+# square root of 7
+# natural logarithm of 5
+# sin of 90 degrees
+###
+import math
+
+square_root = math.sqrt(7)
+print('A square root of 7 is', square_root)
