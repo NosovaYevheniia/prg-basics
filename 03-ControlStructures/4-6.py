252ca1d4136ece47b91c74146de11e84b0025e9a
Yevheniia Nosova <zhenianosova2006@gmail.com>
2025-10-30 19:43:13 +0100

    new 5

---
+++ 03-ControlStructures/4-6.py
@@ -0,0 +1,11 @@
+###
+# Calculates the sum of integer numbers in the range <1,5>
+# sum the numbers from 5 to 10
+# use the += operator in the expression sum=sum+i
+#
+sum = 0
+
+for i in range(1,6):
+    sum = sum + i
+
+print(f'Sum is {sum}')
