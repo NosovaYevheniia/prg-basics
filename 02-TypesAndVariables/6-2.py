834a362c4c83514d753fcdb96c43317e722c0694
Yevheniia Nosova <zhenianosova2006@gmail.com>
2025-11-06 23:55:17 +0100

    new

---
+++ 02-TypesAndVariables/6-2.py
@@ -0,0 +1,6 @@
+###
+# A program that prints your initials
+#
+name = 'Yevheniia'
+surname = "Nosova"
+print(name[0],surname[0])
---
+++ 02-TypesAndVariables/6-3.py
@@ -0,0 +1,8 @@
+###
+# A program that prints a university abbreviation
+#   
+university = "Krakow University of Economics"
+city = "Krakow"
+uni = "University"
+what = "Economics"
+print()
