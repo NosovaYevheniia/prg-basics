287e4195adb6d1905163ae486a513432b4ba2a8a
Yevheniia Nosova <zhenianosova2006@gmail.com>
2025-11-13 19:37:39 +0100

    new

---
+++ 07-Arrays/1-6.py
@@ -0,0 +1,8 @@
+###
+# Returns the name of the day of the week for a given day number (1-Monday ... 7-Sunday)
+#1, 4, 7
+#
+def weekday(n):
+      weekdays = ["Monday", "Tuesday", "Wednesday",
+         "Thursday", "Friday", "Saturday", "Sunday"]
+   return weekdays[]
