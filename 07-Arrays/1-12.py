76382fa7e0e8d5604e5493b8279cceb71ee6f8ac
Yevheniia Nosova <zhenianosova2006@gmail.com>
2025-11-27 13:01:20 +0100

    3 new

---
+++ 07-Arrays/1-12.py
@@ -0,0 +1,10 @@
+# Monthly expenses and their corresponding expense categories are included in the arrays below. 
+# Write a program that calculates which expense category was the most expensive.
+
+categories = ["Food", "Transport", "Rent","Entertainment"]
+expenses = [500, 150, 1000, 200]
+
+expenses_max = max(expenses)
+maxIndex = expenses.index(expenses_max)
+categories_max = categories[maxIndex]
+print("The most expensive categorie is:", categories_max)
